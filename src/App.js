29a9da96e9c50b1c81d50185535ad9cf53750b0c
Ankit <Ankit@bestpeers.com>
2021-04-14 19:33:02 +0530

    Custom-Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Fragment } from "react";
 import "./App.css";
+import CustomHook from "./CustomHook";
 import HooksComp from "./HooksComp";
 function App() {
+  const [salary, increamentSalary] = CustomHook()
+  const [salary2, increamentSalary2] = CustomHook()
   return (
     <Fragment>
       <h2>Hello World APP.JS</h2>
       <hr />
-      <HooksComp />
+      <h2>Employee Details</h2>
+      <h2>Hii User Your salary is {salary}</h2>
+      <button onClick={increamentSalary}>Increament Salary</button>
+      <h2>Hii User Your salary is {salary2}</h2>
+      <button onClick={increamentSalary2}>Increament Salary</button>
     </Fragment>
   );
 }
